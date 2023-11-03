f0108ecb73859155836c6d7e0e25b85f82f49b22
Jordan-Walters-23 <128825567+Jordan-Walters-23@users.noreply.github.com>
2023-11-03 14:39:14 +0000

    testing live

---
+++ main.js
@@ -0,0 +1,73 @@
+console.log("--------CONNECT TO THE MAIN.JS--------");
+const bodyContainer = document.querySelector(".body-container");
+// Header Container
+const headerContainer = document.querySelector(".header-container");
+
+// Note Container--------------------------------
+const noteContainer = document.querySelector(".note-container");
+
+// Questions Written Block-- Start of a Written Block
+const qsWrittenBlock = document.querySelector(".qs-written-block");
+// Questions Written Block Prompt
+const qsWrittenBlockPrompt = document.querySelector(".qs-written-block-prompt");
+// Questions Written Block Textarea
+const qsWrittenBlockTextarea = document.querySelector(
+  ".qs-written-block-textarea"
+);
+// Written Block input field
+
+// Questions entries are Number and linked via IDs
+// title
+const titleEntry = document.querySelector("#title-box");
+// note
+const noteEntry = document.querySelector("#body-box");
+
+// CREATE: POST/Fetch Request to the Server
+async function sendDataToDatabase(e) {
+  e.preventDefault();
+
+  // Store all the new Data from the question section
+  const data = new FormData(noteContainer);
+  /* note: emoji response value defined elsewhere */
+  let titleEntryValue = titleEntry.value || "No Title provided.";
+  let noteEntryValue = noteEntry.value || "No note provided.";
+
+  // !!!Check if the Submit is working
+  console.log("Input2: " + titleEntryValue);
+  console.log("Input3: " + noteEntryValue);
+
+  // Empty all the question Written blocks
+  noteContainer.reset();
+
+  // Send a Fetch/POST Request to the Server
+  const response = await fetch("http://localhost:5000/note/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: titleEntryValue,
+      note: noteEntryValue,
+    }),
+  });
+
+  // If Statement - Check if response.ok is true
+  if (response.ok) {
+    // Store the json response
+    const data = await response.json();
+    // !!!Check if the data is correct
+    console.log("POST: " + data);
+    // Popup to notify the user, new entry sent
+    // displayPopUpForNewData();
+  } else {
+    // console.log error message
+    const error = await response.text();
+    console.log(error);
+  }
+  // Update the Journal Area with new Data
+  //   getAllDataFromServer();
+}
+
+// Event Listeners //////////////////////////////////////////////////////////////////////////////
+// Questions Container/POST Form
+noteContainer.addEventListener("Done", sendDataToDatabase);
